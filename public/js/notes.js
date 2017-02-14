c299ca06b68271a61840aa8f36b5fc300b4d154e
Ajs Ro 12 <ajs-ro-12@ptc-web011-017.ptc.local>
2017-02-14 13:37:38 +0200

    Add hardcoded notest

---
+++ public/js/notes.js
@@ -0,0 +1,14 @@
+var module = angular.module('myapp', []);
+module.controller('NotesController',
+    function($scope, $http) {
+        $scope.notes = [];
+
+        var update = function() {
+            $http.get("/notes")
+                .success(function(notes) {
+                    $scope.notes = notes;
+                });
+        };
+
+        update();
+    });
--- public/notes.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by ajs-ro-12 on 2/14/2017.
- */
