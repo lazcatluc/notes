76ce89f187165591a54f1980e9eca93ae623cd02
Ajs Ro 12 <ajs-ro-12@ptc-web011-017.ptc.local>
2017-02-15 10:12:55 +0200

    Add store in file

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var MongoStore = require('connect-mongo/es5')(session);
+var fs = require('fs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -17,19 +18,32 @@ app.use(session({
     saveUninitialized: true
 }));
 
+function readNotesFromFile() {
+    return JSON.parse(fs.readFileSync("notes.json", 'utf8'));
+}
+
+function writeNotesToFile(notes) {
+    fs.writeFileSync("notes.json", JSON.stringify(notes), "utf8");
+}
+
 app.get("/notes", function(req, res) {
-    res.send(req.session.notes||[]);
+    res.send(req.session.notes||readNotesFromFile());
 });
 
 app.post("/notes", function(req, res) {
     if (!req.session.notes) {
-        req.session.notes = [];
-        req.session.last_note_id = 0;
+        req.session.notes = readNotesFromFile();
+        req.session.last_note_id = -1;
+        for (note in req.session.notes) {
+            if (note.id > req.session.last_note_id) {
+                req.session.last_note_id = note.id;
+            }
+        }
     }
     var note = req.body;
-    note.id = req.session.last_note_id;
-    req.session.last_note_id++;
+    note.id = ++req.session.last_note_id;
     req.session.notes.push(note);
+    writeNotesToFile(req.session.notes);
     res.end();
 });
 
@@ -43,6 +57,7 @@ app.delete("/notes", function(req,res) {
         }
     }
     req.session.notes = updatedNotesList;
+    writeNotesToFile(req.session.notes);
     res.end();
 });
 
