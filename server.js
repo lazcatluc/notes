d244c59840c98c0a9c27ce53e148c0b8216463d0
Ajs Ro 12 <ajs-ro-12@ptc-web011-017.ptc.local>
2017-02-15 13:41:15 +0200

    Add store in mongo db

--- server.js
+++ server.js
@@ -5,60 +5,44 @@ var session = require('express-session');
 var bodyParser = require('body-parser');
 var MongoStore = require('connect-mongo/es5')(session);
 var fs = require('fs');
+var Db = require('mongodb').Db;
+var Server = require('mongodb').Server;
+var ObjectId = require('mongodb').ObjectID;
+
+var db = new Db('tutor',
+    new Server("localhost", 27017, {safe: true},
+        {auto_reconnect: true}, {}));
+
+db.open(function(){
+    console.log("mongo db is opened!");
+
+    db.collection('notes', function(error, notes) {
+        db.notes = notes;
+    });
+
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(session({
-    store: new MongoStore({
-        url: 'mongodb://localhost:27017/angular_session'
-    }),
-    secret: 'angular_tutorial',
-    resave: true,
-    saveUninitialized: true
-}));
-
-function readNotesFromFile() {
-    return JSON.parse(fs.readFileSync("notes.json", 'utf8'));
-}
-
-function writeNotesToFile(notes) {
-    fs.writeFileSync("notes.json", JSON.stringify(notes), "utf8");
-}
 
 app.get("/notes", function(req, res) {
-    res.send(req.session.notes||readNotesFromFile());
+    db.notes.find(req.query).toArray(function(err, items) {
+        res.send(items);
+    });
 });
 
 app.post("/notes", function(req, res) {
-    if (!req.session.notes) {
-        req.session.notes = readNotesFromFile();
-        req.session.last_note_id = -1;
-        for (note in req.session.notes) {
-            if (note.id > req.session.last_note_id) {
-                req.session.last_note_id = note.id;
-            }
-        }
-    }
-    var note = req.body;
-    note.id = ++req.session.last_note_id;
-    req.session.notes.push(note);
-    writeNotesToFile(req.session.notes);
+    db.notes.insert(req.body);
     res.end();
 });
 
 app.delete("/notes", function(req,res) {
-    var id = req.query.id;
-    var notes = req.session.notes||[];
-    var updatedNotesList = [];
-    for (var i=0;i<notes.length;i++) {
-        if (notes[i].id != id) {
-            updatedNotesList.push(notes[i]);
-        }
-    }
-    req.session.notes = updatedNotesList;
-    writeNotesToFile(req.session.notes);
-    res.end();
+    console.log(req.query.id);
+    db.notes.remove({_id: ObjectId(req.query.id)}, function(err, data) {
+        res.end();
+    });
+
 });
 
 app.listen(3000);
